2ff0646e31d561d53a26f57872660f267b209c9a
kiki3165 <kikikou82@gmail.com>
2023-04-07 13:24:54 +0000

    documentation

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,21 +5,21 @@ import urllib.request
 import json
 import sys
 
-"""API endpoint and employee ID"""
+# API endpoint and employee ID
 url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos"
 
-"""Make API request"""
+# Make API request
 with urllib.request.urlopen(url) as response:
     data = json.loads(response.read().decode())
 
-"""Calculate TODO list progress"""
+# Calculate TODO list progress
 total_tasks = len(data)
 completed_tasks = sum(todo["completed"] for todo in data)
 
-"""Display TODO list progres"""
+# Display TODO list progress
 print(f"Employee {data[0]['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
 
-"""Display completed tasks"""
+# Display completed tasks
 for todo in data:
     if todo["completed"]:
         print(f"\t{todo['title']}")
