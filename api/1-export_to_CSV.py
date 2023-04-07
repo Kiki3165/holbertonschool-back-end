77fa7316a17ad6965207270fec282b1503720c9e
kiki3165 <kikikou82@gmail.com>
2023-04-07 14:14:52 +0000

    test task1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
 """docu"""
+import csv
 import requests
 import sys
-import csv
 
-if __name__ == '__main__':
-    """ gets the employee name """
-    employee_id = int(sys.argv[1])
-    url_id = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    api_response_employee = requests.get(url_id)
-    employee_json = api_response_employee.json()
-    USERNAME = employee_json.get('username')
+employee_id = int(sys.argv[1])
 
-    """ gets the list of tasks """
-    url_todo = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    api_response_todos = requests.get(url_todo)
-    tasks_json = api_response_todos.json()
+"""Get employee name"""
+url_employee = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+response_employee = requests.get(url_employee)
+employee_json = response_employee.json()
+employee_name = employee_json.get('name')
 
-    """ extracts relevant information and writes to CSV file """
-    filename = f'{employee_id}.csv'
-    with open(filename, mode='w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for task in tasks_json:
+"""Get user tasks"""
+url_todo = 'https://jsonplaceholder.typicode.com/todos'
+response_todo = requests.get(url_todo)
+user_tasks_json = response_todo.json()
+
+"""Filter tasks for the given employee ID"""
+employee_tasks = [task for task in user_tasks_json if task.get('userId') == employee_id]
+
+"""Count completed tasks and record task details in CSV format"""
+num_completed_tasks = 0
+with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
+    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for task in employee_tasks:
+        if task.get('completed'):
+            num_completed_tasks += 1
             writer.writerow({
-                'USER_ID': task.get('userId'),
-                'USERNAME': USERNAME,
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
                 'TASK_COMPLETED_STATUS': task.get('completed'),
                 'TASK_TITLE': task.get('title')
             })
-    
-    print(f'TODO list for user {USERNAME} has been saved to {filename}')
+
+"""Print progress information"""
+num_tasks = len(employee_tasks)
+print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{num_tasks}):")
+for task in employee_tasks:
+    if task.get('completed'):
+        print(f"\t {task.get('title')}")
