49660be2a47c7f80a7e5435976dfd000b4aa6678
kiki3165 <kikikou82@gmail.com>
2023-04-07 13:20:14 +0000

    change code

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
 """docu"""
 
-import csv
-import requests
-from sys import argv
+import urllib.request
+import json
+import sys
 
+# API endpoint and employee ID
+url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos"
 
-if __name__ == '__main__':
-    employee_id = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    tasks_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+# Make API request
+with urllib.request.urlopen(url) as response:
+    data = json.loads(response.read().decode())
 
-    user_request = requests.get(user_url)
-    tasks_request = requests.get(tasks_url)
+# Calculate TODO list progress
+total_tasks = len(data)
+completed_tasks = sum(todo["completed"] for todo in data)
 
-    employee_name = user_request.json().get('name')
-    tasks = tasks_request.json()
+# Display TODO list progress
+print(f"Employee {data[0]['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
 
-    total_tasks = len(tasks)
-    completed_tasks = [task for task in tasks if task.get('completed')]
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employee_name, len(completed_tasks), total_tasks))
-    for task in completed_tasks:
-        print('\t {}'.format(task.get('title')))
-
-    with open('{}.csv'.format(employee_id), mode='w') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([employee_id,
-                             employee_name,
-                             task.get('completed'),
-                             task.get('title')])
+# Display completed tasks
+for todo in data:
+    if todo["completed"]:
+        print(f"\t{todo['title']}")
