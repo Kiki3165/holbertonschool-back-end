b99c791343075c794288fea57962696423bc24af
kiki3165 <kikikou82@gmail.com>
2023-04-07 14:18:38 +0000

    test task1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,44 +1,26 @@
 #!/usr/bin/python3
 """docu"""
 import csv
+import json
 import requests
 import sys
 
-employee_id = int(sys.argv[1])
 
-"""Get employee name"""
-url_employee = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-response_employee = requests.get(url_employee)
-employee_json = response_employee.json()
-employee_name = employee_json.get('name')
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
 
-"""Get user tasks"""
-url_todo = 'https://jsonplaceholder.typicode.com/todos'
-response_todo = requests.get(url_todo)
-user_tasks_json = response_todo.json()
+    USER_ID = sys.argv[1]
+    url_id = f'{url}/users/{USER_ID}'
+    api_response_employee = requests.get(url_id)
+    employee_json = api_response_employee.json()
+    USERNAME = employee_json.get('username')
 
-"""Filter tasks for the given employee ID"""
-employee_tasks = [task for task in user_tasks_json if task.get('userId') == employee_id]
+    url_tasks = f'{url}/todos?userId={USER_ID}'
+    api_response_tasks = requests.get(url_tasks)
+    task_json = api_response_tasks.json()
 
-"""Count completed tasks and record task details in CSV format"""
-num_completed_tasks = 0
-with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
-    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for task in employee_tasks:
-        if task.get('completed'):
-            num_completed_tasks += 1
-            writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': employee_name,
-                'TASK_COMPLETED_STATUS': task.get('completed'),
-                'TASK_TITLE': task.get('title')
-            })
-
-"""Print progress information"""
-num_tasks = len(employee_tasks)
-print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{num_tasks}):")
-for task in employee_tasks:
-    if task.get('completed'):
-        print(f"\t {task.get('title')}")
+    with open(f'{USER_ID}.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in task_json:
+                writer.writerow([USER_ID, USERNAME,
+                                 task.get('completed'), task.get('title')])
