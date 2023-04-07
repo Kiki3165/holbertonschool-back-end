f53577064d06aa2dc728232f789a163c25eb0056
kiki3165 <kikikou82@gmail.com>
2023-04-07 14:03:24 +0000

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
 """docu"""
 
+import json
 import requests
 import sys
 
-url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos"
 
-response = requests.get(url)
+if __name__ == '__main__':
+    """ gets the employee name """
+    employee_id = int(sys.argv[1])
+    url_id = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    api_response_employee = requests.get(url_id)
+    employee_json = api_response_employee.json()
+    EMPLOYEE_NAME = employee_json.get('name')
 
-if response.status_code != 200:
-    print("Error: Could not retrieve TODO list data for employee ID", sys.argv[1])
-    sys.exit(1)
+    """ gets the number of tasks completed """
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    api_response_todos = requests.get(url_todo)
+    user_tasks_json = api_response_todos.json()
+    employee_todo = None
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+    for todo in user_tasks_json:
+        if todo.get("userId") == int(employee_id):
+            employee_todo = todo
+            if employee_todo.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo['title'])
+            TOTAL_NUMBER_OF_TASKS += 1
 
-todos = response.json()
-
-total_tasks = len(todos)
-completed_tasks = sum(todo["completed"] for todo in todos)
-
-print(f"Employee {todos[0]['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
-
-for todo in todos:
-    if todo["completed"]:
-        print(f"\t{todo['title']}")
+    print(f"Employee {EMPLOYEE_NAME} is done "
+          f"with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for task in TASK_TITLE:
+        print(f"\t {task}")
